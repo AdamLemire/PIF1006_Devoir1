abc7321d819a9fae293ee18085c32b5c8923e426
AdamLemire <adam_lemire@hotmail.com>
2016-11-08 13:33:37 -0500

    13h30 mardi branche adam

--- PIF1006Devoir1/PIF1006Devoir1/Matrice.cs
+++ PIF1006Devoir1/PIF1006Devoir1/Matrice.cs
@@ -17,22 +17,81 @@ namespace PIF1006Devoir1
             this._matrice = matrice;
         }
 
+        public int GetLength(int dimension)
+        {
+            return _matrice.GetLength(dimension);
+        }
+
+        public double this[int i, int j]
+        {
+            get { return this[i, j];}
+            set { this[i, j] = value; }
+        }
+
+        //méthode de vérification même format de matrice
+        public bool VerifierFormatMatrice(Matrice matrice)
+        {
+            if ( _matrice.GetLength(0) == matrice.GetLength(0) && _matrice.GetLength(1) == matrice.GetLength(1))
+            {
+                return true;
+            }
+
+            else return false;
+        }
+
+        //méthode vérification prérequis multiplication
+        public bool VerifierColonneAcommeLigneB(Matrice matrice)
+        {
+            if (_matrice.GetLength(1) == matrice.GetLength(0))
+            return true;
+            else
+            return false;
+        }
+
+
         //méthode d'addition de matrice
         public Matrice Additionner(Matrice matrice)
         {
-            return null;
+            if (VerifierFormatMatrice(matrice) == true)
+            {
+                Matrice addition = new Matrice(new double[_matrice.GetLength(0) ,_matrice.GetLength(1)]);
+                for (int i = 0; i < matrice.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrice.GetLength(1); j++)
+                    {
+                        addition[i, j] = matrice[i, j] + matrice[i, j];
+                    }
+                }
+                return addition;
+            }
+            else
+            {
+                return null;
+            }
         }
 
         //méthode du produit scalaire
         public Matrice FaireProduitScalaire(double scalaire)
         {
-            return null;
+            int x = _matrice.GetLength(0);
+            int y = _matrice.GetLength(1);
+            Matrice produitScalaire = new Matrice(new double[x,y]);
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    produitScalaire[i, j] = _matrice[i, j] * scalaire;
+                }
+            }
+
+            return produitScalaire;
         }
 
         //méthode du produit matriciel
         public Matrice FaireProduitMatriciel(Matrice matrice)
         {
-            return null;
+            
+                return null;
         }
 
         //méthode vérification si triangulaire
@@ -42,30 +101,59 @@ namespace PIF1006Devoir1
         }
 
         //trace
-        public double Trace()
+        public double Trace
         {
-            get (0);
+            get
+            {
+                return 0;
+            }
         }
 
         //determinant
-        public double Determinant() { }
-
-        public Matrice Transposee()
-        {
+        public double Determinant {
+            get
+            {
+                return 0;
+            }
         }
 
-        public Matrice CoMatrice()
+        public Matrice Transposee
         {
+            get
+            {
+                return null;
+            }
         }
 
-        public Matrice MatriceInverse() { }
-
-        public bool EstCarree()
+        public Matrice CoMatrice
         {
-            
+            get
+            {
+                return null;
+            }
         }
 
-        public bool EstReguliere() { }
+        public Matrice MatriceInverse {
+            get
+            {
+                return null;
+            }
+        }
+
+        public bool EstCarree
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public bool EstReguliere {
+            get
+            {
+                return false;
+            }
+        }
 
 
     }
