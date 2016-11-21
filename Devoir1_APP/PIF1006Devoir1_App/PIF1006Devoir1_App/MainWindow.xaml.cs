b9598c66d9b6766e155f2c8ded1fb01f683aa266
AdamLemire <adam_lemire@hotmail.com>
2016-11-21 11:51:54 -0500

    suite wpf

--- Devoir1_APP/PIF1006Devoir1_App/PIF1006Devoir1_App/MainWindow.xaml.cs
+++ Devoir1_APP/PIF1006Devoir1_App/PIF1006Devoir1_App/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PIF1006Devoir1;
 
 namespace PIF1006Devoir1_App
 {
@@ -20,25 +21,58 @@ namespace PIF1006Devoir1_App
     /// </summary>
     public partial class MainWindow : Window
     {
+        private TextBox[,] matriceA, matriceB, matriceC;
+
+
         public MainWindow()
         {
             InitializeComponent();
-            compose_MatriceA();
+            //compose_MatriceA();
         }
 
-        private List<TextBox> compose_MatriceA()
+        /*private List<TextBox> compose_MatriceA()
         {
             List<TextBox> matriceA5x5 = new List<TextBox>((new TextBox[] { A1_1,A1_2,A1_3,A1_4,A1_5,A2_1,A2_2,A2_3,A2_4,A2_5,A3_1,A3_2,A3_3,A3_4,A3_5,A4_1,A4_2,A4_3,A4_4,A4_5,A5_1,A5_2,A5_3,A5_4,A5_5 }));
             return matriceA5x5;
         }
+        */
 
+        /// <summary>
+            /// Création matrice A 5x5
+            /// </summary>
+            /// <returns></returns>
         private TextBox[,] composeMatrice_MatriceA()
         {
             TextBox[,] matrice5x5 = new TextBox[,] { { A1_1, A1_2, A1_3, A1_4, A1_5 }, { A2_1, A2_2, A2_3, A2_4, A2_5 }, { A3_1, A3_2, A3_3, A3_4, A3_5 }, { A4_1, A4_2, A4_3, A4_4, A4_5 }, { A5_1, A5_2, A5_3, A5_4, A5_5 } } ;
             return matrice5x5;
         }
 
+        /// <summary>
+        /// Création matrice B 5x5
+        /// </summary>
+        /// <returns></returns>
+        private TextBox[,] composeMatrice_MatriceB()
+        {
+            TextBox[,] matrice5x5 = new TextBox[,] { { B1_1, B1_2, B1_3, B1_4, B1_5 }, { B2_1, B2_2, B2_3, B2_4, B2_5 }, { B3_1, B3_2, B3_3, B3_4, B3_5 }, { B4_1, B4_2, B4_3, B4_4, B4_5 }, { B5_1, B5_2, B5_3, B5_4, B5_5 } };
+            return matrice5x5;
+        }
 
+        /// <summary>
+        /// Création matrice C 5x5
+        /// </summary>
+        /// <returns></returns>
+        private TextBox[,] composeMatrice_MatriceC()
+        {
+            TextBox[,] matrice5x5 = new TextBox[,] { { C1_1, C1_2, C1_3, C1_4, C1_5 }, { C2_1, C2_2, C2_3, C2_4, C2_5 }, { C3_1, C3_2, C3_3, C3_4, C3_5 }, { C4_1, C4_2, C4_3, C4_4, C4_5 }, { C5_1, C5_2, C5_3, C5_4, C5_5 } };
+            return matrice5x5;
+        }
+
+        /// <summary>
+        /// composer matrice A
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
         private TextBox[,] compose_MatriceAInput(int x, int y)
         {
             TextBox[,] matriceTmp = composeMatrice_MatriceA();
@@ -50,8 +84,50 @@ namespace PIF1006Devoir1_App
                     matriceA[i, j] = matriceTmp[i, j];
                 }
             }
-                    
+            this.matriceA = matriceA;
             return matriceA;
+        }
+
+        /// <summary>
+        /// composer matrice B
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private TextBox[,] compose_MatriceBInput(int x, int y)
+        {
+            TextBox[,] matriceTmp = composeMatrice_MatriceB();
+            TextBox[,] matriceB = new TextBox[x, y];
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    matriceB[i, j] = matriceTmp[i, j];
+                }
+            }
+            this.matriceB = matriceB;
+            return matriceB;
+        }
+
+        /// <summary>
+        /// composer matrice C
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private TextBox[,] compose_MatriceCInput(int x, int y)
+        {
+            TextBox[,] matriceTmp = composeMatrice_MatriceC();
+            TextBox[,] matriceC = new TextBox[x, y];
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    matriceC[i, j] = matriceTmp[i, j];
+                }
+            }
+            this.matriceC = matriceC;
+            return matriceC;
         }
 
         private void textBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -59,7 +135,8 @@ namespace PIF1006Devoir1_App
 
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        
+        private void iniMatriceA_Click(object sender, RoutedEventArgs e)
         {
             if (LignesA.Text != "" && ColonnesA.Text != "")
             {
@@ -79,7 +156,50 @@ namespace PIF1006Devoir1_App
                 else resultatsTxt.Text = "Nombre de ligne ou colonne trop petit";
             }
             else resultatsTxt.Text = "Erreur du nombre de lignes ou colonnes";
+        }
 
+        private void iniMatriceB_Click(object sender, RoutedEventArgs e)
+        {
+            if (LignesB.Text != "" && ColonnesB.Text != "")
+            {
+                int lignes, colonnes;
+                lignes = Convert.ToInt32(LignesB.Text);
+                colonnes = Convert.ToInt32(ColonnesB.Text);
+
+
+                if (lignes > 0 || colonnes > 0)
+                {
+                    TextBox[,] matrice = compose_MatriceBInput(lignes, colonnes);
+                    foreach (TextBox textBox in matrice)
+                    {
+                        textBox.Visibility = Visibility.Visible;
+                    }
+                }
+                else resultatsTxt.Text = "Nombre de ligne ou colonne trop petit";
+            }
+            else resultatsTxt.Text = "Erreur du nombre de lignes ou colonnes";
+        }
+
+        private void iniMatriceC_Click(object sender, RoutedEventArgs e)
+        {
+            if (LignesC.Text != "" && ColonnesC.Text != "")
+            {
+                int lignes, colonnes;
+                lignes = Convert.ToInt32(LignesC.Text);
+                colonnes = Convert.ToInt32(ColonnesC.Text);
+
+
+                if (lignes > 0 || colonnes > 0)
+                {
+                    TextBox[,] matrice = compose_MatriceCInput(lignes, colonnes);
+                    foreach (TextBox textBox in matrice)
+                    {
+                        textBox.Visibility = Visibility.Visible;
+                    }
+                }
+                else resultatsTxt.Text = "Nombre de ligne ou colonne trop petit";
+            }
+            else resultatsTxt.Text = "Erreur du nombre de lignes ou colonnes";
         }
 
         private void EffacerA_Click(object sender, RoutedEventArgs e)
@@ -94,6 +214,131 @@ namespace PIF1006Devoir1_App
             }
         }
 
+        private void EffacerB_Click(object sender, RoutedEventArgs e)
+        {
+            LignesA.Text = "";
+            ColonnesA.Text = "";
 
+            TextBox[,] matriceB5x5 = composeMatrice_MatriceB();
+            foreach (TextBox textbox in matriceB5x5)
+            {
+                textbox.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void EffacerC_Click(object sender, RoutedEventArgs e)
+        {
+            LignesA.Text = "";
+            ColonnesA.Text = "";
+
+            TextBox[,] matriceC5x5 = composeMatrice_MatriceC();
+            foreach (TextBox textbox in matriceC5x5)
+            {
+                textbox.Visibility = Visibility.Hidden;
+            }
+        }
+
+        /// <summary>
+        /// Création de la matrice A à partir des inputs
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private Matrice CreerMatriceA(int x, int y)
+        {
+            Matrice matriceAPleine = new Matrice(new double[x, y]);
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    matriceAPleine[i, j] = Convert.ToInt32(matriceA[i, j].GetLineText(0));
+                    resultatsTxt.Text = resultatsTxt.Text + " "+ Convert.ToString(matriceAPleine[i,j]);
+                }
+            }
+            return matriceAPleine;
+        }
+
+        /// <summary>
+        /// Création de la matrice B à partir des inputs
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private Matrice CreerMatriceB(int x, int y)
+        {
+            Matrice matriceBPleine = new Matrice(new double[x, y]);
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    matriceBPleine[i, j] = Convert.ToInt32(matriceB[i, j].GetLineText(0));
+                    resultatsTxt.Text = resultatsTxt.Text + " " + Convert.ToString(matriceBPleine[i, j]);
+                }
+            }
+            return matriceBPleine;
+        }
+
+
+        /// <summary>
+        /// Création de la matrice C à partir des inputs
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private Matrice CreerMatriceC(int x, int y)
+        {
+            Matrice matriceCPleine = new Matrice(new double[x, y]);
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    matriceCPleine[i, j] = Convert.ToInt32(matriceC[i, j].GetLineText(0));
+                    //resultatsTxt.Text = resultatsTxt.Text + " " + Convert.ToString(matriceCPleine[i, j]);
+                }
+            }
+            return matriceCPleine;
+        }
+
+        private void BtnAddition_Click(object sender, RoutedEventArgs e)
+        {          
+            Matrice matriceA = CreerMatriceA(Convert.ToInt32(LignesA.Text), Convert.ToInt32(ColonnesA.Text));
+            Matrice matriceB = CreerMatriceB(Convert.ToInt32(LignesB.Text), Convert.ToInt32(ColonnesB.Text));
+            resultatsTxt.Text = (matriceA.Additionner(matriceB)).AfficheMatrice();
+        }
+
+        private void BtnProduitScalaire_Click(object sender, RoutedEventArgs e)
+        {
+            Matrice matriceA = CreerMatriceA(Convert.ToInt32(LignesA.Text), Convert.ToInt32(ColonnesA.Text));
+            resultatsTxt.Text = matriceA.FaireProduitScalaire(Convert.ToInt32(Scalaire.Text)).AfficheMatrice();      
+        }
+
+        private void BtnProduitMatriciel_Click(object sender, RoutedEventArgs e)
+        {
+            Matrice matriceA = CreerMatriceA(Convert.ToInt32(LignesA.Text), Convert.ToInt32(ColonnesA.Text));
+            Matrice matriceB = CreerMatriceB(Convert.ToInt32(LignesB.Text), Convert.ToInt32(ColonnesB.Text));
+            Matrice matriceC;
+            int a = 0;
+            if (LignesC.Text != "" && ColonnesC.Text != "")
+            {
+                matriceC = CreerMatriceC(Convert.ToInt32(LignesC.Text), Convert.ToInt32(ColonnesC.Text));
+                resultatsTxt.Text = (matriceA.FaireProduitMatriciel(out a, matriceB, matriceC)).AfficheMatrice();
+            }
+            else resultatsTxt.Text = (matriceA.FaireProduitMatriciel(out a, matriceB)).AfficheMatrice();
+
+            resultatsTxt.Text += "Nombre de produits effectues : " + a;
+        }
+
+        private void Scalaire_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            tb.Text = string.Empty;
+            tb.GotFocus -= Scalaire_GotFocus;
+        }
+
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            CreerMatriceB(Convert.ToInt32(LignesB.Text), Convert.ToInt32(ColonnesB.Text));
+        }
     }
 }
---
+++ Devoir1_APP/PIF1006Devoir1_App/PIF1006Devoir1_App/Systeme.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIF1006Devoir1
+{
+    class Systeme
+    {
+        //initialisation
+        private Matrice _matriceCarree, _matrice1N;
+
+        //constructeur
+        public Systeme(Matrice matriceCarree, Matrice matrice1N)
+        {
+            if (matriceCarree.EstCarree && matrice1N.GetLength(1) == 1 && matriceCarree.GetLength(0) == matrice1N.GetLength(0))
+            {
+                _matriceCarree = matriceCarree;
+                _matrice1N = matrice1N;
+            }
+            else
+            {
+                Console.WriteLine("Format de matrice incorrect");
+            }
+
+
+        }
+
+        //propriété de _matriceCarree
+        public Matrice MatriceCarree
+        {
+            get { return _matriceCarree; }
+            set { _matriceCarree = value; }
+        }
+
+        //propriété de _matrice1N
+        public Matrice Matrice1N
+        {
+            get { return _matrice1N; }
+            set { _matrice1N = value; }
+        }
+
+        //Méthode de Cramer
+        public Matrice TrouverXParCramer()
+        {
+            if (_matriceCarree.Determinant != 0)
+            {
+                int n = _matriceCarree.GetLength(0);
+                Matrice X = new Matrice((new double[n, 1]));
+                Matrice AN = new Matrice(new double[n, n]);
+
+                for (int i = 0; i < n; i++)
+                {
+                    AN = _matriceCarree.CopierMatrice();
+                    for (int j = 0; j < n; j++)
+                    {
+                        AN[j, i] = _matrice1N[j, 0];
+                    }
+                    X[i, 0] = AN.Determinant / _matriceCarree.Determinant;
+                }
+                return X;
+            }
+
+            else
+            {
+                Console.WriteLine("Déterminant de la matrice = 0, donc impossible d'utiliser la méthode de Cramer");
+                return new Matrice(new double[0, 0]);
+            }
+        }
+
+        //Méthode d'inversion matricielle
+        public Matrice TrouverXParInversionMatricielle()
+        {
+            if (_matriceCarree.Determinant != 0)
+            {
+                int n = _matriceCarree.GetLength(0);
+                Matrice X = new Matrice((new double[n, 1]));
+                X = (_matriceCarree.MatriceInverse).FaireProduitMatriciel(_matrice1N);
+                return X;
+            }
+            else
+            {
+                Console.WriteLine("Déterminant de la matrice = 0, donc impossible d'utiliser la méthode de d'inversion matricielle");
+                return new Matrice(new double[0, 0]);
+            }
+        }
+
+        //Méthode de Jacobi
+        public Matrice TrouverXParJacobi(double epsilon)
+        {
+            int n = _matriceCarree.GetLength(0);
+            Matrice d = new Matrice((new double[n, 1]));
+            Matrice xDroite = new Matrice((new double[n, n + 1])); //
+            Matrice xGauche = new Matrice((new double[n, 1]));
+            Matrice xGaucheAvant = new Matrice((new double[n, 1]));
+            bool continuer = true;
+
+            //création de la matrice colonne des valeurs de la diagonale et instanciation de matrice x gauche
+            for (int i = 0; i < n; i++)
+            {
+                d[i, 0] = _matriceCarree[i, i];
+                xGauche[i, 0] = 0;
+            }
+
+            //création de la matrice des formules X
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i != j)
+                    {
+                        xDroite[i, j] =  - _matriceCarree[i, j]/d[i, 0];
+                    }
+                    else
+                    {
+                        xDroite[i, j] = 0;
+                    }
+                    
+                }
+                xDroite[i, n] = _matrice1N[i, 0]/d[i, 0];
+            }
+
+
+            //itération pour trouver X 
+            while (continuer)
+            {
+                bool continuerTemp = false;
+                for (int i = 0; i < n; i++)
+                {
+                    xGaucheAvant[i, 0] = xGauche[i, 0];
+                    xGauche[i, 0] = 0;
+                    
+
+                    for (int j = 0; j < n; j++)
+                    {
+                        xGauche[i, 0] += xDroite[i, j] * xGaucheAvant[j, 0];
+                    }
+                    xGauche[i, 0] += xDroite[i, n];
+                    Console.WriteLine(xGauche[i, 0]);
+                    if (Math.Abs(xGauche[i, 0] - xGaucheAvant[i, 0]) >= epsilon || continuerTemp)
+                    {
+                        continuerTemp = true;
+                    }
+                    //else continuerTemp = false;
+                }
+                continuer = continuerTemp;
+
+            }
+            return xGauche;
+        }
+
+        //propriété strictement dominante diagonalement
+        private bool DominanteDiag
+        {
+            get
+            {
+                double somme = 0; //somme des a(i,j) pour j!= i
+                for (int i = 0; i < _matriceCarree.GetLength(0); i++)
+                {
+                    for (int j = 0; j < _matriceCarree.GetLength(0); j++)
+                    {
+                        if (i != j)
+                        {
+                            somme += Math.Abs(_matriceCarree[i, j]);
+                        }
+                    }
+                }
+                for (int i = 0; i < _matriceCarree.GetLength(0); i++)
+                {
+                    if (Math.Abs(_matriceCarree[i, i]) > somme) { return true; }
+                }
+                return false;
+            }
+        }
+
+        //propriété irréductiblement dominante diagonalement
+
+
+    }
+}
