19aaea227d294c07fa62b1090eae0204f73b0579
AdamLemire <adam_lemire@hotmail.com>
2016-11-21 16:11:19 -0500

    suite

--- Devoir1_APP/PIF1006Devoir1_App/PIF1006Devoir1_App/MainWindow.xaml.cs
+++ Devoir1_APP/PIF1006Devoir1_App/PIF1006Devoir1_App/MainWindow.xaml.cs
@@ -27,6 +27,7 @@ namespace PIF1006Devoir1_App
         public MainWindow()
         {
             InitializeComponent();
+            CacherCases();
             //compose_MatriceA();
         }
 
@@ -38,12 +39,16 @@ namespace PIF1006Devoir1_App
         */
 
         /// <summary>
-            /// Création matrice A 5x5
-            /// </summary>
-            /// <returns></returns>
+        /// Création matrice A 5x5
+        /// </summary>
+        /// <returns></returns>
         private TextBox[,] composeMatrice_MatriceA()
         {
-            TextBox[,] matrice5x5 = new TextBox[,] { { A1_1, A1_2, A1_3, A1_4, A1_5 }, { A2_1, A2_2, A2_3, A2_4, A2_5 }, { A3_1, A3_2, A3_3, A3_4, A3_5 }, { A4_1, A4_2, A4_3, A4_4, A4_5 }, { A5_1, A5_2, A5_3, A5_4, A5_5 } } ;
+            TextBox[,] matrice5x5 = new TextBox[,]
+            {
+                {A1_1, A1_2, A1_3, A1_4, A1_5}, {A2_1, A2_2, A2_3, A2_4, A2_5}, {A3_1, A3_2, A3_3, A3_4, A3_5},
+                {A4_1, A4_2, A4_3, A4_4, A4_5}, {A5_1, A5_2, A5_3, A5_4, A5_5}
+            };
             return matrice5x5;
         }
 
@@ -53,7 +58,11 @@ namespace PIF1006Devoir1_App
         /// <returns></returns>
         private TextBox[,] composeMatrice_MatriceB()
         {
-            TextBox[,] matrice5x5 = new TextBox[,] { { B1_1, B1_2, B1_3, B1_4, B1_5 }, { B2_1, B2_2, B2_3, B2_4, B2_5 }, { B3_1, B3_2, B3_3, B3_4, B3_5 }, { B4_1, B4_2, B4_3, B4_4, B4_5 }, { B5_1, B5_2, B5_3, B5_4, B5_5 } };
+            TextBox[,] matrice5x5 = new TextBox[,]
+            {
+                {B1_1, B1_2, B1_3, B1_4, B1_5}, {B2_1, B2_2, B2_3, B2_4, B2_5}, {B3_1, B3_2, B3_3, B3_4, B3_5},
+                {B4_1, B4_2, B4_3, B4_4, B4_5}, {B5_1, B5_2, B5_3, B5_4, B5_5}
+            };
             return matrice5x5;
         }
 
@@ -63,7 +72,11 @@ namespace PIF1006Devoir1_App
         /// <returns></returns>
         private TextBox[,] composeMatrice_MatriceC()
         {
-            TextBox[,] matrice5x5 = new TextBox[,] { { C1_1, C1_2, C1_3, C1_4, C1_5 }, { C2_1, C2_2, C2_3, C2_4, C2_5 }, { C3_1, C3_2, C3_3, C3_4, C3_5 }, { C4_1, C4_2, C4_3, C4_4, C4_5 }, { C5_1, C5_2, C5_3, C5_4, C5_5 } };
+            TextBox[,] matrice5x5 = new TextBox[,]
+            {
+                {C1_1, C1_2, C1_3, C1_4, C1_5}, {C2_1, C2_2, C2_3, C2_4, C2_5}, {C3_1, C3_2, C3_3, C3_4, C3_5},
+                {C4_1, C4_2, C4_3, C4_4, C4_5}, {C5_1, C5_2, C5_3, C5_4, C5_5}
+            };
             return matrice5x5;
         }
 
@@ -76,8 +89,8 @@ namespace PIF1006Devoir1_App
         private TextBox[,] compose_MatriceAInput(int x, int y)
         {
             TextBox[,] matriceTmp = composeMatrice_MatriceA();
-            TextBox[,] matriceA = new TextBox[x,y];
-            for (int i = 0; i< x; i++)
+            TextBox[,] matriceA = new TextBox[x, y];
+            for (int i = 0; i < x; i++)
             {
                 for (int j = 0; j < y; j++)
                 {
@@ -135,7 +148,7 @@ namespace PIF1006Devoir1_App
 
         }
 
-        
+
         private void iniMatriceA_Click(object sender, RoutedEventArgs e)
         {
             if (LignesA.Text != "" && ColonnesA.Text != "")
@@ -143,15 +156,16 @@ namespace PIF1006Devoir1_App
                 int lignes, colonnes;
                 lignes = Convert.ToInt32(LignesA.Text);
                 colonnes = Convert.ToInt32(ColonnesA.Text);
-                
+
 
                 if (lignes > 0 || colonnes > 0)
                 {
                     TextBox[,] matrice = compose_MatriceAInput(lignes, colonnes);
-                    foreach(TextBox textBox in matrice)
+                    foreach (TextBox textBox in matrice)
                     {
                         textBox.Visibility = Visibility.Visible;
                     }
+                    Nombres1.Visibility = Visibility.Visible;
                 }
                 else resultatsTxt.Text = "Nombre de ligne ou colonne trop petit";
             }
@@ -174,6 +188,7 @@ namespace PIF1006Devoir1_App
                     {
                         textBox.Visibility = Visibility.Visible;
                     }
+                    Nombres2.Visibility = Visibility.Visible;
                 }
                 else resultatsTxt.Text = "Nombre de ligne ou colonne trop petit";
             }
@@ -196,10 +211,36 @@ namespace PIF1006Devoir1_App
                     {
                         textBox.Visibility = Visibility.Visible;
                     }
+                    Nombres3.Visibility = Visibility.Visible;
                 }
                 else resultatsTxt.Text = "Nombre de ligne ou colonne trop petit";
             }
             else resultatsTxt.Text = "Erreur du nombre de lignes ou colonnes";
+        }
+
+        private void CacherCases()
+        {
+            TextBox[,] matriceA5x5 = composeMatrice_MatriceA();
+            TextBox[,] matriceB5x5 = composeMatrice_MatriceB();
+            TextBox[,] matriceC5x5 = composeMatrice_MatriceC();
+            foreach (TextBox textbox in (matriceA5x5))
+            {
+                textbox.Text = "";
+                textbox.Visibility = Visibility.Hidden;
+            }
+            foreach (TextBox textbox in (matriceB5x5))
+            {
+                textbox.Text = "";
+                textbox.Visibility = Visibility.Hidden;
+            }
+            foreach (TextBox textbox in (matriceC5x5))
+            {
+                textbox.Text = "";
+                textbox.Visibility = Visibility.Hidden;
+            }
+            Nombres1.Visibility = Visibility.Hidden;
+            Nombres2.Visibility = Visibility.Hidden;
+            Nombres3.Visibility = Visibility.Hidden;
         }
 
         private void EffacerA_Click(object sender, RoutedEventArgs e)
@@ -210,32 +251,38 @@ namespace PIF1006Devoir1_App
             TextBox[,] matriceA5x5 = composeMatrice_MatriceA();
             foreach (TextBox textbox in matriceA5x5)
             {
+                textbox.Text = "";
                 textbox.Visibility = Visibility.Hidden;
             }
+            Nombres1.Visibility = Visibility.Hidden;
         }
 
         private void EffacerB_Click(object sender, RoutedEventArgs e)
         {
-            LignesA.Text = "";
-            ColonnesA.Text = "";
+            LignesB.Text = "";
+            ColonnesB.Text = "";
 
             TextBox[,] matriceB5x5 = composeMatrice_MatriceB();
             foreach (TextBox textbox in matriceB5x5)
             {
                 textbox.Visibility = Visibility.Hidden;
+                textbox.Text = "";
             }
+            Nombres2.Visibility = Visibility.Hidden;
         }
 
         private void EffacerC_Click(object sender, RoutedEventArgs e)
         {
-            LignesA.Text = "";
-            ColonnesA.Text = "";
+            LignesC.Text = "";
+            ColonnesC.Text = "";
 
             TextBox[,] matriceC5x5 = composeMatrice_MatriceC();
             foreach (TextBox textbox in matriceC5x5)
             {
                 textbox.Visibility = Visibility.Hidden;
+                textbox.Text = "";
             }
+            Nombres3.Visibility = Visibility.Hidden;
         }
 
         /// <summary>
@@ -252,7 +299,7 @@ namespace PIF1006Devoir1_App
                 for (int j = 0; j < y; j++)
                 {
                     matriceAPleine[i, j] = Convert.ToInt32(matriceA[i, j].GetLineText(0));
-                    resultatsTxt.Text = resultatsTxt.Text + " "+ Convert.ToString(matriceAPleine[i,j]);
+                    resultatsTxt.Text = resultatsTxt.Text + " " + Convert.ToString(matriceAPleine[i, j]);
                 }
             }
             return matriceAPleine;
@@ -300,7 +347,7 @@ namespace PIF1006Devoir1_App
         }
 
         private void BtnAddition_Click(object sender, RoutedEventArgs e)
-        {          
+        {
             Matrice matriceA = CreerMatriceA(Convert.ToInt32(LignesA.Text), Convert.ToInt32(ColonnesA.Text));
             Matrice matriceB = CreerMatriceB(Convert.ToInt32(LignesB.Text), Convert.ToInt32(ColonnesB.Text));
             resultatsTxt.Text = (matriceA.Additionner(matriceB)).AfficheMatrice();
@@ -309,7 +356,7 @@ namespace PIF1006Devoir1_App
         private void BtnProduitScalaire_Click(object sender, RoutedEventArgs e)
         {
             Matrice matriceA = CreerMatriceA(Convert.ToInt32(LignesA.Text), Convert.ToInt32(ColonnesA.Text));
-            resultatsTxt.Text = matriceA.FaireProduitScalaire(Convert.ToInt32(Scalaire.Text)).AfficheMatrice();      
+            resultatsTxt.Text = matriceA.FaireProduitScalaire(Convert.ToInt32(Scalaire.Text)).AfficheMatrice();
         }
 
         private void BtnProduitMatriciel_Click(object sender, RoutedEventArgs e)
@@ -326,6 +373,26 @@ namespace PIF1006Devoir1_App
             else resultatsTxt.Text = (matriceA.FaireProduitMatriciel(out a, matriceB)).AfficheMatrice();
 
             resultatsTxt.Text += "Nombre de produits effectues : " + a;
+        }
+
+        private void BtnTriangularite_Click(object sender, RoutedEventArgs e) {
+            Matrice matriceA = CreerMatriceA(Convert.ToInt32(LignesA.Text), Convert.ToInt32(ColonnesA.Text));
+            bool stricte = Stricte.IsChecked.Value;
+            int choix;
+            string reponse = "Non";
+            if (radioButton1.IsChecked.Value) choix = 0;
+            else if (radioButton2.IsChecked.Value) choix = 1;
+            else choix = 2;
+            bool m = matriceA.EstTriangulaire(choix, stricte);
+            if (m == true) reponse = "Oui";
+            resultatsTxt.Text = "Triangulaire tel que spécifié? : "+reponse;
+        }
+
+        private void Trace_Click(object sender, RoutedEventArgs e)
+        {
+            Matrice matriceA = CreerMatriceA(Convert.ToInt32(LignesA.Text), Convert.ToInt32(ColonnesA.Text));
+            double trace = matriceA.Trace;
+            resultatsTxt.Text = "La trace de la matrice A est : " + trace;
         }
 
         private void Scalaire_GotFocus(object sender, RoutedEventArgs e)
